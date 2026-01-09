072d0b40efe6d8bfec4b45836ec084d42c6d50ca
norris-devs <newmannorris09@gmail.com>
2026-01-09 20:52:30 +0100

    add Mini Greeting App file

---
+++ My first ever Mini Greeting App.py
@@ -0,0 +1,23 @@
+print("Mini Greeting App - Part 2 - Getting Acquainted")
+
+name = input("Welcome, would you like to tell me what your name is? ")
+
+print("Nice to meet you " + name + "!")
+
+color = input("Tell me your favorite color ")
+
+if color == "Blue":
+    print("What coincidence! Blue is my favorite color too!")
+    
+else:
+    print("Interesting. I never would have guessed that.")
+    
+food = input("What is your favorite food? ")
+
+if food == "Pizza":
+    print("Bingo! Who doesn't love Pizzzaaa! Haha.")
+    
+else:
+    print("Not a bad choice at all.")
+    
+print("It was nice getting acquainted " + name + "!" + " Hope to see you again soon!")
